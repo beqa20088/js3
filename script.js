f9718976569957b0c247811b8db003f516713666
beqa20088 <138366831+beqa20088@users.noreply.github.com>
2024-08-07 19:43:05 +0400

    asdasda

---
+++ script.js
@@ -0,0 +1,22 @@
+let temrature = 21
+console.log(temrature === 0);
+if (temrature <= 10) {
+        console.log('yinavs');
+} else if (temrature === 11,12,13,14,15,16,17,18,19,20){
+        console.log('civa');
+}else if (temrature === 21){
+        console.log('tbila');
+} else{
+        console.log('cxela');
+    }
+//bevri gza vcade da ver davwere da ras vaketeb araswroad es mexute davalebisaa da ver vigeb meotxe davalebasac qvemot davwer
+let age=17
+console.log(age===18);
+if (age<=17){
+    console.log('tqven ar shegizliat martvis mowmobis ageba');
+} else if (age >=18){
+    console.log('tqven shegizliat');
+} else{
+    console.log('mititebuli asaki arasworia');
+} 
+// es meotxe davaleba
